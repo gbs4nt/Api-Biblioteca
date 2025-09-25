3a0548afff75b241a2875defe1ab785d01db26ba
Gabriel <glima9719@gmail.com>
2025-09-25 14:59:27 -0300

    feat: criar controllers com endpoints iniciais

--- src/main/java/com/biblioteca/digital/controller/LivroController.java
+++ src/main/java/com/biblioteca/digital/controller/LivroController.java
@@ -1,4 +1,43 @@
 package com.biblioteca.digital.controller;
 
+
+import com.biblioteca.digital.model.CriarLivroDTO;
+import com.biblioteca.digital.model.Livro;
+import com.biblioteca.digital.service.LivroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
+
+    private LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> criarLivro(@RequestBody CriarLivroDTO criarLivroDTO){
+      var livroId =  livroService.criarLivro(criarLivroDTO);
+
+        return ResponseEntity.created(URI.create("/v1/livros/"+livroId.toString())).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscarLivro(@PathVariable("id") String id){
+
+        return null;
+    }
+
+    @PutMapping("/{usuarioId}/{livroId}/emprestar")
+    public ResponseEntity<Void> emprestarLivro(@PathVariable("usuarioId") String usuarioId,
+                                               @PathVariable("livroId") String livroId){
+        livroService.alugarLivro(usuarioId,livroId);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/com/biblioteca/digital/controller/UsuarioController.java
+++ src/main/java/com/biblioteca/digital/controller/UsuarioController.java
@@ -1,4 +1,34 @@
 package com.biblioteca.digital.controller;
 
+
+import com.biblioteca.digital.model.CriarUsuarioDTO;
+import com.biblioteca.digital.model.Usuario;
+import com.biblioteca.digital.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody CriarUsuarioDTO criarUsuarioDTO){
+
+
+      var usuarioId =  usuarioService.criarUsuario(criarUsuarioDTO);
+
+        return ResponseEntity.created(URI.create("/usuarios/"+usuarioId.toString())).build();
+
+    }
 }
